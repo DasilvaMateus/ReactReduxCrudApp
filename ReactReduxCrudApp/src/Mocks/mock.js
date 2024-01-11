719be85a5013ad285b798099eb7423191ce711a9
Mateus Natanael <natinael@hotmail.com>
2024-01-11 10:33:54 -0300

    app

---
+++ ReactReduxCrudApp/src/Mocks/mock.js
@@ -0,0 +1,17 @@
+export const userList = [
+{
+    name:'Mateus Natanael',
+    email:"mateus.natanael.silva97@gmail.com",
+    id:1
+},
+{
+    name:'Mateus Silva',
+    email:"mateus.silva97@gmail.com",
+    id:2
+},
+{
+    name:'Tsorig',
+    email:"Tsorig@gmail.com",
+    id:3
+},
+]
